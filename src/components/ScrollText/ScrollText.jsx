51d9af60451e58ca7dd916cd2695831af301e059
PraWater <praneel.maddula@gmail.com>
2024-06-19 17:35:48 +0530

    fix: scrolling

--- src/components/ScrollText/ScrollText.jsx
+++ src/components/ScrollText/ScrollText.jsx
@@ -3,18 +3,34 @@ import "./scrollText.css";
 export default function ScrollText({ onTitleEnter, onTitleExit }) {
   return (
     <div className="scroll-text">
-      <p onMouseEnter={onTitleEnter} onMouseOut={onTitleExit}>
+      <div
+        className="scroll-text-p scroll-top"
+        onMouseEnter={onTitleEnter}
+        onMouseOut={onTitleExit}
+      >
         <span>building the next generation of gaming</span> · building the next
-        generation of gaming
-      </p>
-      <p onMouseEnter={onTitleEnter} onMouseOut={onTitleExit}>
+        generation of gaming ·{" "}
+        <span>building the next generation of gaming</span> · building the next
+        generation of gaming ·{" "}
+      </div>
+      <div
+        className="scroll-text-p scroll-middle"
+        onMouseEnter={onTitleEnter}
+        onMouseOut={onTitleExit}
+      >
         redefining the future of xr games ·{" "}
-        <span>redefining the future of xr games</span>
-      </p>
-      <p onMouseEnter={onTitleEnter} onMouseOut={onTitleExit}>
+        <span>redefining the future of xr games</span> · redefining the future
+        of xr games · <span>redefining the future of xr games</span> ·{" "}
+      </div>
+      <div
+        className="scroll-text-p scroll-bottom"
+        onMouseEnter={onTitleEnter}
+        onMouseOut={onTitleExit}
+      >
         <span>VR experience should be limitless</span> · VR experience should be
-        limitless
-      </p>
+        limitless · <span>VR experience should be limitless</span> · VR
+        experience should be limitless ·{" "}
+      </div>
     </div>
   );
 }
