f253b9b667cbcd5a388ccb0b2d0f5e5489850c6c
PraWater <praneel.maddula@gmail.com>
2024-07-14 15:31:46 +0530

    fix: scrollText component

--- src/components/ScrollText/ScrollText.jsx
+++ src/components/ScrollText/ScrollText.jsx
@@ -1,36 +1,34 @@
+import React from "react";
 import "./scrollText.css";
 
 export default function ScrollText({ onTitleEnter, onTitleExit }) {
+  const lines = [
+    "building the next generation of gaming",
+    "we are redefining the future of xr games",
+    "Your VR experience should be limitless",
+  ];
+
   return (
     <div className="scroll-text">
-      <div
-        className="scroll-text-p scroll-top"
-        onMouseEnter={onTitleEnter}
-        onMouseOut={onTitleExit}
-      >
-        <span>building the next generation of gaming</span> · building the next
-        generation of gaming ·{" "}
-        <span>building the next generation of gaming</span> · building the next
-        generation of gaming ·{" "}
-      </div>
-      <div
-        className="scroll-text-p scroll-middle"
-        onMouseEnter={onTitleEnter}
-        onMouseOut={onTitleExit}
-      >
-        redefining the future of xr games ·{" "}
-        <span>redefining the future of xr games</span> · redefining the future
-        of xr games · <span>redefining the future of xr games</span> ·{" "}
-      </div>
-      <div
-        className="scroll-text-p scroll-bottom"
-        onMouseEnter={onTitleEnter}
-        onMouseOut={onTitleExit}
-      >
-        <span>VR experience should be limitless</span> · VR experience should be
-        limitless · <span>VR experience should be limitless</span> · VR
-        experience should be limitless ·{" "}
-      </div>
+      {lines.map((line) => (
+        <div
+          className="scroll-text-wrap"
+          onMouseEnter={onTitleEnter}
+          onMouseOut={onTitleExit}
+          key={line}
+        >
+          {[1, 2].map((_, index) => (
+            <div className="scroll-text-p" key={index}>
+              {[true, false, true, false].map((isGradient, i) => (
+                <React.Fragment key={i}>
+                  <span className={isGradient ? "gradient" : ""}>{line}</span>
+                  {i < 3 && <span>&middot;</span>}
+                </React.Fragment>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
